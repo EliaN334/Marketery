740d589b5221302c846e20636987cc36bcb24337
EliaN334 <72530539+EliaN334@users.noreply.github.com>
2023-02-02 18:57:41 -0500

    fixed header export

--- src/components/atoms/Header.tsx
+++ src/components/atoms/Header.tsx
@@ -1,10 +1,14 @@
-import { type Header, HeaderOne, HeaderTwo } from './Headers';
+import {
+  type Header as MainHeaderProps,
+  HeaderOne,
+  HeaderTwo,
+} from './Headers';
 import type { HeaderOneProps } from './Headers/HeaderOne';
 import type { HeaderTwoProps } from './Headers/HeaderTwo';
 
 type HeaderLevel = HeaderOneProps | HeaderTwoProps;
 
-type HeaderProps = HeaderLevel & Header;
+type HeaderProps = HeaderLevel & MainHeaderProps;
 
 const Header: React.FC<HeaderProps> = ({ heading, ...rest }) => {
   return heading === 'h1' ? <HeaderOne {...rest} /> : <HeaderTwo {...rest} />;
